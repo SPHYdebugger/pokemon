81d6384c3f7740211b55afc1cb99d9d682179fd5
sphy <sphy.debugger@gmail.com>
2023-03-25 20:55:38 +0100

    funcionando como quiero

--- js/javascript.js
+++ js/javascript.js
@@ -2,17 +2,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
     
     // metodo para obtener los datos
     const getListPokemon = async () => {
-        const url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0';
+        const url = 'https://pokeapi.co/api/v2/pokemon?limit=50&offset=0';
 
 
         const result = await fetch(url);
         const listResult = await result.json();
         console.log(listResult.results);
+        createPokemonItem(listResult.results);
 
     }
     // metodo para el HTML
-    const createPokemonItem = () => {
-    
+    const createPokemonItem = (results) => {
+        console.log('createPokemonItem =>', results);
+        console.log('createPokemonItem name=>', results.name);
+        const poke_list = document.querySelector('#poke_list');
+
+        results.forEach(element => {
+            console.log('foreach =>', element.name);
+            const item = document.createElement('p');
+        item.classList.add('list-pokemon_item');
+        poke_list.appendChild(item);
+
+        item.innerText = element.name;
+        });
+        
     
     
     }
