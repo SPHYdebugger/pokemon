0fe93e199bad65547cdbba6a47b55b10eb90ce46
sphy <sphy.debugger@gmail.com>
2023-03-26 01:15:26 +0100

    modificado a pokemon

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     
     // metodo para obtener los datos
     const getListPokemon = async () => {
-        const url = 'https://pokeapi.co/api/v2/pokemon?limit=50&offset=0';
+        const url = 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0';
 
 
         const result = await fetch(url);
@@ -11,6 +11,67 @@ window.addEventListener('DOMContentLoaded', (event) => {
         createPokemonItem(listResult.results);
 
     }
+
+
+
+
+/*
+    const createPokemonItem = (results) => {
+        console.log('createPokemonItem =>', results);
+        console.log('createPokemonItem name=>', results.name);
+        const poke_list = document.querySelector('#poke_list');
+
+        //results.forEach(element => {
+          //  console.log('foreach =>', element.name);
+            const item = document.createElement('div');
+        item.classList.add('pokemon_card');
+        
+        
+        var name = results.name;
+	    let card_option2_content = `
+        <div class="card opacity-75" style="width: 18rem;">
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">+ INFO</a>
+        </div>
+      </div>
+	`;
+	poke_list.innerHTML = card_option2_content;
+    poke_list.appendChild(item);
+
+
+
+        
+       // });
+        
+    
+    
+    }
+
+    getListPokemon();
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+    
     // metodo para el HTML
     const createPokemonItem = (results) => {
         console.log('createPokemonItem =>', results);
